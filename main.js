918f461c30bdb5bbbd8a2d7f47b6627419432752
star-coder-sammu <101115549+star-coder-sammu@users.noreply.github.com>
2022-12-17 19:22:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+song = "";
+song2 = "";
+song3 = "";
+
+function preload()
+{
+    song = loadSound("inten.mp3");
+	song2 = loadSound("music.mp3");
+	song3 = loadSound("wrap.mp3");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function setup() {
 	canvas =  createCanvas(600, 500);
 	canvas.center();
